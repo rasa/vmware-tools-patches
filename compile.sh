2324619d0402ac0b431844a2e26682507b200524
Ross Smith II <ross@smithii.com>
2015-03-12 08:39:56 -0700

    Add perl to required packages

--- compile.sh
+++ compile.sh
@@ -13,7 +13,7 @@ if hash vmware-uninstall-tools.pl >/dev/null 2>&1; then
   sudo vmware-uninstall-tools.pl
 fi
 
-if [ -e /etc/redhat-release ]; then
+if [[ -e /etc/redhat-release ]]; then
 	if hash yum >/dev/null 2>&1; then
 		sudo yum -y install gcc glibc-headers kernel-devel kernel-headers make perl
 	fi
@@ -21,7 +21,7 @@ fi
 
 if [[ -e /etc/debian_version ]]; then
 	if hash apt-get >/dev/null 2>&1; then
-		sudo apt-get install -y build-essential dkms linux-headers-$(uname -r) patch psmisc
+		sudo apt-get install -y build-essential dkms linux-headers-$(uname -r) patch perl psmisc
 	fi
 fi
 
