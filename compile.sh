981d3b4190256b6605d4801195a3fed2368e1fb4
dragon788 <dragon788@users.noreply.github.com>
2016-01-03 12:39:13 -0600

    Adding service for systemd for vmware-tools

--- compile.sh
+++ compile.sh
@@ -29,3 +29,8 @@ fi
 sudo ./vmware-install.pl --default ${VMWARE_INSTALL_OPTIONS}
 
 popd >/dev/null
+
+if hash systemctl >/dev/null 2>&1; then
+  sudo cp $SCRIPT_DIR/patches/vmware-tools.service /etc/systemd/system/
+  sudo systemctl enable vmware-tools.service
+fi
